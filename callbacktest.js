079d2c5d22afd70bd0f820c4bdcaf90e03a90e10
Name <audtjs4055@naver.com>
2022-02-11 16:15:09 +0900

    test

--- callbacktest.js
+++ callbacktest.js
@@ -1,15 +1,15 @@
-function run() {
-    const start = Date.now();
-    for (let i = 0; i < 1000000000; i++) {}
-    const end = Date.now();
-    console.log(end - start + 'ms');
-  }
+// function run() {
+//     const start = Date.now();
+//     for (let i = 0; i < 1000000000; i++) {}
+//     const end = Date.now();
+//     console.log(end - start + 'ms');
+//   }
  
-  run();
-  console.log('Done !!!');
+//   run();
+//   console.log('Done !!!');
 
 
-  
+
 // function run() {
 //   setTimeout(() => {
 //     const start = Date.now();
